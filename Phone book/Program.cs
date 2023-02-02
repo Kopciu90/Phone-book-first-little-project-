6d47e1490a0e0839441f796b5b07ccfcdf2d207f
Kopciu90 <piotrekkopson@wp.pl>
2023-02-02 19:58:02 +0100

    Add project files.

---
+++ Phone book/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Phone_book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
